a444f1664303678fbd9dd7f14b8695d6360ff887
robertbean1 <49346300+robertbean1@users.noreply.github.com>
2019-07-09 16:11:04 -0400

    Add files via upload

--- NEAT/NEAT.py
+++ NEAT/NEAT.py
@@ -1,61 +1,20 @@
 from Genes import Genome, Genepool
 from Genes.backend import *
 from Network import FeedForwardNetwork
+from Species import Population
 
-class Species(Genepool):
-    def __init__(self, ancestor, *args, **kwargs):
-        
-        super().__init__(*args, **kwargs)
+P = Population()
+P.generate_members(Genome(2, 2), 50)
 
-        self.ancestor = ancestor
-        self.members = []
-        self.add_member(self.ancestor)
-        
-        
-    def add_members(self, members):
-        self.package_genomes(members)
-        for member in members:
-            self.members.append(member)
-
-    def add_member(self, member):
-        self.package_genome(member)
-        self.members.append(member)
-    
-    def is_member(self, potential_member, delta_t, *args, **kwargs):
-        return self.topological_similarity(self.ancestor, potential_member, *args, delta_t=delta_t, **kwargs)
-
-    def mean_topological_similarity(self, potential_member, *args, **kwargs):
-        similarity = 0
-        for member in self.members:
-            similarity += self.topological_similarity(member, potential_member, *args, **kwargs)
-        return similarity / len(self.members)
-
-    def generate_members(self, size):
-        for _ in range(size):
-            self.members.append(self.ancestor.binary_fission(1, 0.2, 0.2, 0.2))
-
-class Population:
-    def __init__(self, species, species_members, inputs, outputs):
-        self.niches = []
-        for _ in range(species):
-            niche = Species(Genome(inputs, outputs))
-            niche.generate_members(species_members)
-            self.niches.append(niche)
-
-S = Species(Genome(2, 1))
-S.generate_members(50)
-
-for member in S.members:
+for member in P.members:
     Net = FeedForwardNetwork(member)
     print(Net.predict([0, 1]))
-
-print(S.mean_topological_similarity(Genome(2, 1)))
 
 #To do - Breeding:
 #
 #          _/    1) topological comparison
 #
-#          0.5_/ 2) speciation (We're halfway there, ooooh oh, we're living on a prayer)
+#          _/    2) speciation
 #
 #          X     3) crossing over
 #
---
+++ NEAT/Species/species.py
@@ -0,0 +1,31 @@
+from Genes import Genome, Genepool
+from Genes.backend import *
+
+class Population(Genepool):
+    def __init__(self, *args, **kwargs):
+        
+        super().__init__(*args, **kwargs)
+        
+        self.members = []
+        
+    def add_members(self, members):
+        self.package_genomes(members)
+        for member in members:
+            self.members.append(member)
+
+    def add_member(self, member):
+        self.package_genome(member)
+        self.members.append(member)
+
+    def generate_members(self, ancestor, size):
+        for _ in range(size):
+            self.members.append(ancestor.binary_fission(1, 1, 1, 1))
+
+    def adjust_fitness(self, fitness, subject_member, delta_t, c1=1, c2=1, c3=0.1):
+        n = 1
+        for member in self.members:
+            if self.topological_similarity(subject_member, member, c1, c2, c3, delta_t):
+                n+=1
+        
+        return fitness / n
+            
